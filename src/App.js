39e42b45903daf19e346a66cb2375057d4c1bbc1
Jeong-jeong <ghkdvnfld345@naver.com>
2021-03-30 10:38:28 +0900

    클래스 스타일과 함수 스타일의 차이, hook

--- src/App.js
+++ src/App.js
@@ -1,65 +1,61 @@
-import { Component } from 'react';
-import Toc from './Components/Toc'
-import Content from './Components/Content'
-import Subject from './Components/Subject'
+import React, { useState } from 'react';
 import './App.css';
 
+function App () {
+  return (
+    <div className = 'App'>
+        <h1>Hello World</h1>
+        <FuncComp initNumber = {2}></FuncComp>
+        <ClassComp initNumber = {2}></ClassComp>
+    </div>
+  )
+}
 
+function FuncComp (props) {
+  let numberState = useState(props.initNumber);
+  let number = numberState[0]; // useState의 [0] 요소 = props.initNumber
+  let setNumber = numberState[1]; // useState의 [1] 요소 = 함수
+  // let dateState = useState(new Date().toString());
+  // let date = dateState[0];
+  // let setDate = dateState[1];
+  let [_date, setDate] = useState(new Date().toString())
+  return (
+    <div className = 'container'>
+      <h2>function style component</h2>
+      <p>Number : {number}</p>
+      <p>Today : {_date}</p>
+      <input type = 'button' value = 'random' onClick = {
+          function () {
+            setNumber(Math.random()) // useState의 [1] 함수에 인자로 넣어줌.
+          } 
+        }></input>
+      <input type = 'button' value = 'time' onClick = {
+          function () {
+            setDate(new Date().toString()) // useState의 [1] 함수에 인자로 넣어줌.
+          } 
+        }></input>
+    </div>
+  );
+}
 
-class App extends Component { // React의 Component를 상속받아 App 클래스를 만듦.
-  constructor(props) { 
-    super(props); // state 값 초기화
-    this.state = { 
-      mode: 'read', // 'welcome' 모드일 때
-      selected_content_id: 1,
-      subject: {title: 'WEB', sub: 'world wide web!'},
-      welcome: {title: 'Welcome', desc: 'Hello, React!'}, // welcome 모드일 때 갖는 값
-      contents: [
-        {id: 1, title: 'HTML', desc: 'HTML is for information'},
-        {id: 2, title: 'CSS', desc: 'CSS is for desgin'},
-        {id: 3, title: 'JS', desc: 'JS is for interactive'},
-      ]
-    }
+class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber,
+    date: new Date().toString(),
   }
-  render() { // ⭐️ 클래스 안의 메서드는 function 키워드 생략 가능
-    console.log('App render');
-    let _title, _desc = null;
-    if (this.state.mode === 'welcome') { // welcome 모드일 때 화면 바뀌기
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-    } else if (this.state.mode === 'read') {
-      this.state.contents.forEach ( (item) => {
-        if (item.id === this.state.selected_content_id) {
-          _title = item.title;
-          _desc = item.desc;
-        }
-      } )
-      
-    }
-    console.log('render', this); // App 컴포넌트 > render () 안에서 this = App 
+  render() {
     return (
-      <>
-      <div className='App'>
-        <Subject title={this.state.subject.title} 
-                  sub={this.state.subject.sub}
-                  onChangePage = {function () {
-                    this.setState({mode: 'welcome'});
-                  }.bind(this)}
-        >
-                  
-        </Subject>
-        <Toc onChangePage = { function(id) {
-          this.setState({
-            mode: 'read',
-            selected_content_id: +id
-          });
-        }.bind(this)} 
-        data={this.state.contents}
-        ></Toc>
-        <Content title={_title} desc={_desc}></Content>
+      <div className = 'container'>
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <p>Today : {this.state.date}</p>
+        <input type = 'button' value = 'random' onClick = {
+          function () {
+            this.setState({number: Math.random()})
+          }.bind(this)
+        }></input>
       </div>
-      </>
-    );
+    )
   }
 }
 
