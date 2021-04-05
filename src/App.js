a1f63e8fd4fc4085467a74d38994aed141d05d27
Jeong-jeong <ghkdvnfld345@naver.com>
2021-04-05 15:45:12 +0900

    💬 Learn usage of useRef() to memorable values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, { useRef } from 'react';
 import Counter from './Counter';
 import Hello from './Hello';
 import InputSample from './InputSample';
+import UserList from './UseList';
 import Wrapper from './Wrapper';
 
 
 function App() {
+  const users = [
+		{
+			id: 1,
+			username: 'velopert',
+			email: 'velopert@naver.com'
+		},
+		{
+			id: 2,
+			username: 'Jeongs',
+			email: 'Jeongs@naver.com'
+		},
+		{
+			id: 3,
+			username: 'tester',
+			email: 'tester@naver.com'
+		}
+  ];
+  
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+
+    console.log(nextId.current); // 4
+    nextId.current += 1;
+
+  }
+
   return (
     <div>
       <Wrapper>
@@ -15,6 +44,8 @@ function App() {
       <Counter />
 
       <InputSample />
+
+      <UserList users={users}/>
 
     </div>
   );
